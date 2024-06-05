a863ce3773a20036e942a18a51f7072a8608d9ca
MDRUHULAMIN7 <ruhulthisis@gmail.com>
2024-06-06 00:43:19 +0600

    approve

--- src/Pages/Dashboard/Admin/ApprovedContactRequest.jsx
+++ src/Pages/Dashboard/Admin/ApprovedContactRequest.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import UseAxiosPublic from "../../../Hooks/UseAxiosPublic";
 // import { useState } from "react";
 import UseAuth from "../../../Hooks/UseAuth";
+import Heading from "../Sidebar/Heading";
 
 
 const ApprovedContactRequest = () => {
@@ -23,30 +24,41 @@ const {user}=UseAuth()
 
         console.log(alldata?.length);
 
+   const handleApprove=async(id)=>{
+    const update =`aprovefor${id}`
+    console.log(update);
+    
 
+   }
 
 
     return (
         <div>
+
+            <Heading heading={'Approve Contact Request'} subheading={'approve requested contact data ....'}> </Heading>
                      <div className="overflow-x-auto">
   <table className="table">
     {/* head */}
-    <thead>
+    <thead className="bg-rose-100">
       <tr>
-        <th></th>
+       
         <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
+        <th>Email</th>
+        <th>BiodataId</th>
+        <th>Status</th>
+        <th>Action</th>
       </tr>
     </thead>
     <tbody>
     
-      <tr className="bg-base-200">
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
-      </tr>
+     {alldata?.map((data)=> <tr key={data._id} className="bg-rose-50 mt-2">
+        <th>{data?.name}</th>
+        <td>{data?.email}</td>
+        <td>{data?.biodataId}</td>
+        <td  className=""> <p className={data?.status === "pending" ? "bg-red-500 w-fit  px-3 py-2 txt-lg rounded-xl" : "bg-green-500 p-2 w-fit  px-3 py-2 txt-lg rounded-xl"}>{data?.status}</p> </td>
+        <td > {data?.status ==='pending' ?  <button onClick={()=>handleApprove(data?.biodataId)} className="bg-green-500 p-2 w-fit  px-3 py-2 txt-lg rounded-xl">Approve</button>: <p className="bg-red-500 p-2 w-fit  px-3 py-2 txt-lg rounded-xl">Approved</p>}</td>
+        <hr />
+      </tr> )}
      
     </tbody>
   </table>
