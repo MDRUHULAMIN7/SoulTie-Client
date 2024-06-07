7b01f520ee48d7858b8cc5962dd7c52299e4ba5d
MDRUHULAMIN7 <ruhulthisis@gmail.com>
2024-06-07 21:39:13 +0600

    admin home chart

--- src/Pages/Dashboard/Admin/AdminHome.jsx
+++ src/Pages/Dashboard/Admin/AdminHome.jsx
@@ -23,7 +23,7 @@ const AdminHome = () => {
     return (
         <div>
            <section className="p-6 my-6 dark:bg-gray-100 dark:text-gray-800">
-	<div className="container grid grid-cols-1 gap-2 mx-auto sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 ">
+	<div className="container grid grid-cols-1 gap-2 mx-auto sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4j xl:grid-cols-5 ">
 		<div className="flex p-2 rounded-lg text-rose-400 w-fit dark:bg-gray-50 dark:text-gray-800">
 			<div className="flex justify-center p-2 align-middle rounded-lg sm:p-4 dark:bg-violet-600">
 				<img className="h-16" src={img1} alt="" />
